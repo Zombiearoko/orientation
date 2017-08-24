c27537b67353ba8603f24e9888278d4348146eae
brandon kenedy <kenedagha@yahoo.comm>
2017-08-24 11:26:40 +0200

    correction de bugs générés suite à mon pull

--- backend/src/main/java/com/bocobi2/orientation/model/PaymentMethod.java
+++ backend/src/main/java/com/bocobi2/orientation/model/PaymentMethod.java
@@ -8,32 +8,23 @@ public class PaymentMethod {
 	
 	@Id
 	String paymentMethodId;
-<<<<<<< HEAD
 	String paymentMethodName;
-=======
-	String paymentMetodName;
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
-	
+
 	public PaymentMethod(){}
 	
 	
 
 
 
-<<<<<<< HEAD
+
 	public PaymentMethod(String paymentMethodName) {
 		super();
 		this.paymentMethodName = paymentMethodName;
-=======
-	public PaymentMethod(String paymentMetodName) {
-		super();
-		this.paymentMetodName = paymentMetodName;
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
+
 	}
 
-
 	/**
-<<<<<<< HEAD
+
 	 * @return the paymentMethodName
 	 */
 	public String getpaymentMethodName() {
@@ -45,19 +36,7 @@ public class PaymentMethod {
 	 */
 	public void setpaymentMethodName(String paymentMethodName) {
 		this.paymentMethodName = paymentMethodName;
-=======
-	 * @return the paymentMetodName
-	 */
-	public String getPaymentMetodName() {
-		return paymentMetodName;
-	}
 
-	/**
-	 * @param paymentMetodName the paymentMetodName to set
-	 */
-	public void setPaymentMetodName(String paymentMetodName) {
-		this.paymentMetodName = paymentMetodName;
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
 	}
 
 
@@ -66,14 +45,10 @@ public class PaymentMethod {
 
     @Override
     public String toString() {
-        return String.format(
-<<<<<<< HEAD
-                "{\"paymentMetodId\":%s, \"paymentMethodName\":'%s'}",
-                paymentMethodId, paymentMethodName);
-=======
+        return String.format( 
                 "{\"paymentMetodId\":%s, \"paymentMetodName\":'%s'}",
-                paymentMethodId, paymentMetodName);
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
+                paymentMethodId, paymentMethodName);
+
     }
 
 	
--- backend/src/main/java/com/bocobi2/orientation/repositories/ScholarshipRepository.java
+++ backend/src/main/java/com/bocobi2/orientation/repositories/ScholarshipRepository.java
@@ -8,12 +8,7 @@ import com.bocobi2.orientation.model.Scholarship;
 
 public interface ScholarshipRepository extends MongoRepository<Scholarship, String> {
 
-<<<<<<< HEAD
     public Scholarship findByscholarshipType(String scholarshipType);
     public List<Scholarship> findByscholarshipPublishingDate(String scholarshipPublishingDate);
-=======
-    public Scholarship findByScholarshipCalenderType(String ScholarshipCalenderType);
-    public List<Scholarship> findByScholarshipCalenderYear(String ScholarshipCalenderYear);
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
 
 }
