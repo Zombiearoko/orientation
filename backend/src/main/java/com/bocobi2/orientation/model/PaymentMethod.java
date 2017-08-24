5240da579a25924e1d6487347237ef2dc1d2709d
innocentTialo <tialoinnocent@gmail.com>
2017-08-24 05:05:03 +0100

    supression des depots a conflit

--- backend/src/main/java/com/bocobi2/orientation/model/PaymentMethod.java
+++ backend/src/main/java/com/bocobi2/orientation/model/PaymentMethod.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.bocobi2.orientation.model;
 
 import org.springframework.data.annotation.Id;
@@ -52,87 +51,6 @@ public class PaymentMethod {
 	
 	
 }
-=======
-package com.bocobi2.orientation.model;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "paymentMethod")
-public class PaymentMethod {
-	
-	@Id
-	String paymentMethodId;
-<<<<<<< HEAD
-	String paymentMethodName;
-=======
-	String paymentMetodName;
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
-	
-	public PaymentMethod(){}
-	
 	
 
 
-
-<<<<<<< HEAD
-	public PaymentMethod(String paymentMethodName) {
-		super();
-		this.paymentMethodName = paymentMethodName;
-=======
-	public PaymentMethod(String paymentMetodName) {
-		super();
-		this.paymentMetodName = paymentMetodName;
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
-	}
-
-
-	/**
-<<<<<<< HEAD
-	 * @return the paymentMethodName
-	 */
-	public String getpaymentMethodName() {
-		return paymentMethodName;
-	}
-
-	/**
-	 * @param paymentMethodName the paymentMethodName to set
-	 */
-	public void setpaymentMethodName(String paymentMethodName) {
-		this.paymentMethodName = paymentMethodName;
-=======
-	 * @return the paymentMetodName
-	 */
-	public String getPaymentMetodName() {
-		return paymentMetodName;
-	}
-
-	/**
-	 * @param paymentMetodName the paymentMetodName to set
-	 */
-	public void setPaymentMetodName(String paymentMetodName) {
-		this.paymentMetodName = paymentMetodName;
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
-	}
-
-
-
-
-
-    @Override
-    public String toString() {
-        return String.format(
-<<<<<<< HEAD
-                "{\"paymentMetodId\":%s, \"paymentMethodName\":'%s'}",
-                paymentMethodId, paymentMethodName);
-=======
-                "{\"paymentMetodId\":%s, \"paymentMetodName\":'%s'}",
-                paymentMethodId, paymentMetodName);
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
-    }
-
-	
-	
-	
-}
->>>>>>> 9bff8494973e510009f02e6104232262582327f1
--- backend/src/main/java/com/bocobi2/orientation/repositories/CounselingCenterRepository.java
+++ backend/src/main/java/com/bocobi2/orientation/repositories/CounselingCenterRepository.java
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-package com.bocobi2.orientation.repositories;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.bocobi2.orientation.model.CounselingCenter;
-
-public interface CounselingCenterRepository extends MongoRepository<CounselingCenter, String> {
-
-
-=======
 package com.bocobi2.orientation.repositories;
 
 import java.util.List;
@@ -22,11 +9,6 @@ import com.bocobi2.orientation.model.CounselingCenter;
 public interface CounselingCenterRepository extends MongoRepository<CounselingCenter, String> {
 
     public CounselingCenter findBycounselingSector(String counselingSector);
-<<<<<<< HEAD
     public List<CounselingCenter> findBycounselingCenterName(String counselingCenterName);
-=======
-    public List<CounselingCenter> findBycounselingName(String counselingName);
->>>>>>> 1305f807e59d31c3eb7107fcee1b2e8a68e0f2d0
 
->>>>>>> 9bff8494973e510009f02e6104232262582327f1
 }
